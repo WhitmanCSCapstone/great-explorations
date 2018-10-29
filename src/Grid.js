e6f311faf172d62002199950eaff0f2b30cb7b76
mjkohl32 <mjkohl32@gmail.com>
2018-10-29 15:44:24 -0700

    Added for loop for workshop data

--- src/Grid.js
+++ src/Grid.js
@@ -2,44 +2,78 @@ import React from 'react';
 import { Row, Col, Popover } from 'antd';
 import './Grid.css';
 import WorkshopMenu from './WorkshopMenu.js';
+import { SHEET_DATA, SHEET_LOAD } from "./SheetUtil";
 
 class WorkshopGrid extends React.Component {
+    state = {
+    people: [],
+    error: null,
+    show: false
+    }
+
+    initClient = () => {
+    window.gapi.client
+      .init({
+        apiKey: SHEET_DATA.apiKey,
+        discoveryDocs: SHEET_DATA.discoveryDocs
+      })
+      .then(() => {
+      SHEET_LOAD(this.onLoad);
+    });
+    };
+
+    onLoad = (data, error) => {
+    if (data) {
+      const people = data.people;
+      this.setState({ people });
+    } else {
+      this.setState({ error });
+    }
+    };
+
+    componentDidMount() {
+    window.gapi.load("client", this.initClient);
+    }
+
+    createWorkshops = () => {
+        let table = []
+
+        // Outer loop to create parent
+        for (let i = 0; i < 5; i++) {
+            let children = []
+            //Inner loop to create children
+            for (let j = 1; j < 7; j++) {
+                let mynum = 6 * i + j;
+                children.push(<Col className="gutter-row" span={4}>
+                              <div className="gutter-box"> Workshop {mynum} </div>
+                              </Col>
+                             )
+            }
+            //Create the parent and add the children
+            table.push(<Row gutter={60}>{children}</Row>
+                      )
+        }
+        return table
+    }
+    
     render() {
         const mycontent = (
             <div>
-                <p>Instructor(s)</p>
-                <p>Description</p>
+                <p>{this.state.people.map((person, i) => (
+                <div>{person.instructor}</div>
+              ))}</p>
+                <p>{this.state.people.map((person, i) => (
+                <div>{person.description}</div>
+              ))}</p>
             </div>
         );
         const mytagline = "Tagline";
-        const myworkshopName = "Title of Workshop";
+        const myworkshopName = "Workshop Title";
         
         return (
             <div className="gutter-example">
-                <Row gutter={50}>
-                  <Col className="gutter-row" span={8}>
-                    <WorkshopMenu/>
-                  </Col>
-                  <Col className="gutter-row" span={8}>
-                    <Popover content={mycontent} title={ mytagline } trigger="hover" overlayStyle={{ width: "300px" }}>
-                        <div className="gutter-box"> { myworkshopName } </div>
-                    </Popover>
-                  </Col>
-                  <Col className="gutter-row" span={8}>
-                    <div className="gutter-box"> Workshop 3 </div>
-                  </Col>
-                </Row>
-                <Row gutter={50}>
-                  <Col className="gutter-row" span={8}>
-                    <div className="gutter-box"> Workshop 4 </div>
-                  </Col>
-                  <Col className="gutter-row" span={8}>
-                    <div className="gutter-box"> Workshop 5 </div>
-                  </Col>
-                  <Col className="gutter-row" span={8}>
-                    <div className="gutter-box"> Workshop 6 </div>
-                  </Col>
-                </Row>
+                {this.createWorkshops()}
+            
             </div>
         )
     }
