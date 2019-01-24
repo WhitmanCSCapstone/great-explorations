f8061c9b4f58993173d72be8d984de4c745e1b61
Kirk Lange <kirklange@pm.me>
2019-01-24 01:08:31 -0800

    Updated sheed id and api key

--- src/components/SheetUtil.js
+++ src/components/SheetUtil.js
@@ -2,9 +2,11 @@
 
 export var SHEET_INFO = {
   apiKey: "AIzaSyBIe-MGvrJ2560PsAgDJhbou_ggIa3Ys6A",
+// Switch to apiKey below eventually. For some reason it's not working now... (1/24/2019 12AM)
+//  apiKey: "AIzaSyDQD6bkeo-BpLKpL3IVlQotsbJ1uKP6_XE",
   discoveryDocs: 
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-  spreadsheetId: "1h-UJlgXtuydbPonqGLyPRx3FG8hp9XANk_TxjoQ46uw"
+  spreadsheetId: "12zh88Vmnqh5L2xKJySklLD2ZkIlr0vv9OepqLMp4rT0"
 };
 
 export function SHEET_LOAD(callback) {
