bc14908de36d416aaf1e478e5d34d7c392789357
mjkohl32 <mjkohl32@gmail.com>
2019-01-30 21:03:26 -0800

    slight modifications to sponsors page

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -11,9 +11,9 @@ class Sponsors extends Component {
                         Our Sponsors
                     </p>
                     <p className="App-text-subheader">
-                        Great Explorations in Education thanks the following sponsors for their generous support of the 2019 Great Explorations in Science, Technology, Engineering and Math Conference.<br></br><br></br>
+                        We would like to thank the following sponsors for their generous support of the 2019 Great Explorations in Science, Technology, Engineering and Math Conference.<br></br><br></br>
                         <ul className="App-text">
-                        <li> American Association of University Women - Walla Walla </li>
+                        <li> American Association of University Women (<a href="https://wallawalla-wa.aauw.net">AAUW</a>) Walla Walla</li>
                         <li> YWCA of Walla Walla </li>
                         <li> Walla Walla Public Schools </li>
                         <li> Whitman College </li>
@@ -38,10 +38,6 @@ class Sponsors extends Component {
                         <li> Dr. Janet Davis and the Senior Capstone Project team for developing and creating the new online registration website. </li> 
                         </ul>
                       </p>
-                      <p>
-                        To learn more, visit: <a href="https://wallawalla-wa.aauw.net">wallawalla-wa.aauw.net</a>
-
-                    </p>
                 </div>
 
             </div>
