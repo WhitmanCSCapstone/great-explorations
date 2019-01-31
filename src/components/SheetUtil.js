2a92e1febd446122c32eb468656e586cc2c698fe
Kirk Lange <kirklange@pm.me>
2019-01-30 19:34:19 -0800

    Added new sheet ids

--- src/components/SheetUtil.js
+++ src/components/SheetUtil.js
@@ -2,12 +2,12 @@
 
 export var SHEET_INFO = {
   apiKey: "AIzaSyBIe-MGvrJ2560PsAgDJhbou_ggIa3Ys6A",
-// Switch to apiKey below eventually. For some reason it's not working now... (1/24/2019 12AM)
-//  apiKey: "AIzaSyDQD6bkeo-BpLKpL3IVlQotsbJ1uKP6_XE",
+// Switch to apiKey below eventually. For some reason it's not working now... (1/30/2019 7PM)
+//  apiKey: "AIzaSyDb5ZXM6uhamvy-SZ80g7gME_oebjO_IZs",
   discoveryDocs: 
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
   spreadsheetId: "12zh88Vmnqh5L2xKJySklLD2ZkIlr0vv9OepqLMp4rT0",
-  textsheetId: "1wJ5Bfu0MV9ZXWkrJOv0ltSBCQgRPGFARALKmfPG7uaM"
+  textsheetId: "1f9T3uKqF24H3wOBKweVgFqViDA5dvBOhXsf25zFLXek"
 };
 
 export var LANGUAGES = {
