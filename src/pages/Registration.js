80b0e8de8d0688e472ac8b75028fd19734429b31
mjkohl32 <mjkohl32@gmail.com>
2019-02-02 14:29:43 -0800

    added easier payment routing

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { BackTop } from 'antd';
-import { Anchor } from 'antd';
+import ReactDOM from 'react-dom';
+import { Link, Route } from 'react-router';
+import { BackTop, message, Button } from 'antd';
 import './../App.css';
 import EmbedForm from './../components/EmbedForm.js';
 import WorkshopGrid from './../components/Grid.js';
 import ImageScroll from './../components/ImageScroll.js';
 
-const { Link } = Anchor;
-
 class Registration extends Component {
+    
+    success = () => {
+      message.success('Thank you for registering for Great Explorations! Please use one of the below options to pay the $5 fee.', 10);
+    };
     
     render() {
         return (
@@ -20,9 +23,6 @@ class Registration extends Component {
                     <p className="App-header-text">
                         Welcome to Great Explorations!<br></br>
                         <center style={{ fontSize: '2vw', color: 'black', fontWeight: '400'}}>Scroll down to register </center>
-                    </p>
-                    <p>
-                        <BackTop />
                     </p>
                     <p className="App-text">
                         <center>A Science, Technology, Engineering and Math conference for 5th thru 8th grade girls.<br></br></center>
@@ -37,6 +37,15 @@ class Registration extends Component {
                     </p>
                     <p className="App-header-text">
                         <EmbedForm />
+                    </p>
+                    <p>
+                        <center>
+                            <Button type="primary" onClick={this.success}>
+                                <Link to={"/payment"}>
+                                   {"Please click here after hitting submit!"}
+                                </Link>
+                            </Button>
+                        </center>
                     </p>
                     
                 </div>
