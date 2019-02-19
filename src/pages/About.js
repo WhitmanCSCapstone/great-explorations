c869ae05913dfa2354e5784b5218b0f26a599d8e
Kirk Lange <kirklange@pm.me>
2019-02-18 19:52:30 -0800

    Added brochure pdfs

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 import './../App.css';
 import { WEBTEXT, WEBTEXT_ADD_CALLBACK } from '../components/SheetUtil';
+import brochureEn from './../brochures/GE2019En.pdf';
+import brochureEs from './../brochures/GE2019Es.pdf';
 
 class About extends Component {
 
@@ -21,6 +24,13 @@ class About extends Component {
         return (
             <div style={{ background: '#fff', padding: 24, minHeight: '100vh' }}>
                 <div className="App-header-text">
+                    <p className="App-text">
+                        <center style={{ fontSize: '1.25vw', color: 'black', fontWeight: '200'}}>
+<a target="_blank" href={brochureEn}>English Brochure Download</a>
+{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+<a target="_blank" href={brochureEs}>Descargar Folleto en Español</a>
+</center>
+                    </p>
                     <p className="App-header-text">
                       {this.state.text[17]}
                     </p>
